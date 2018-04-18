e1a458e14218fab6114dde5bfae7bc2cdecb00c5
Louis S. Berman <louis@squideyes.com>
2018-04-18 07:54:55 -0400

    Add MailGateway.sln.licenseheader to "Docs" solution folder

--- Source/Common/MiscHelpers.cs
+++ Source/Common/MiscHelpers.cs
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with MailGateway.  If not, see <http://www.gnu.org/licenses/>.
 
-
 using Toolbelt.Net.Smtp;
 
 namespace Common
--- Source/Common/WellKnown.cs
+++ Source/Common/WellKnown.cs
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with MailGateway.  If not, see <http://www.gnu.org/licenses/>.
 
-
 namespace Common
 {
     public class WellKnown
